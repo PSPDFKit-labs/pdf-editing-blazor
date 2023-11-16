ea5f22057ee9b952b21170b0322f68da5fd63687
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2023-11-16 18:26:37 -0500

    first commit

---
+++ PdfEditor.cs
@@ -0,0 +1,27 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing;
+using PdfSharp.Fonts;
+using PdfSharp.Snippets.Font;
+using PdfSharp;
+
+public class PdfEditor
+{
+    static PdfEditor()
+    {
+        if (Capabilities.Build.IsCoreBuild)
+            GlobalFontSettings.FontResolver = new FailsafeFontResolver();
+    }
+    public void EditPdf(string filePath)
+    {
+
+        PdfDocument document = PdfReader.Open(filePath, PdfDocumentOpenMode.Modify);
+        PdfPage page = document.Pages[0];
+        XGraphics gfx = XGraphics.FromPdfPage(page);
+
+        XFont font = new XFont("Verdana", 20, XFontStyleEx.Bold);
+        gfx.DrawString("Added this with PDFsharp", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.TopLeft);
+
+        document.Save(filePath);
+    }
+}
